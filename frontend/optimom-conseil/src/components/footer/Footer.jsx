603f0582b5e7bdfa99bb7e3ad8c94550a8d74623
Auriane Viggiano <auriane.vigg@gmail.com>
2025-07-16 14:59:44 +0200

    design footer

--- frontend/optimom-conseil/src/components/footer/Footer.jsx
+++ frontend/optimom-conseil/src/components/footer/Footer.jsx
@@ -7,62 +7,59 @@ function Footer () {
       {/* Version Desktop */}
       
       <div className="footer-desktop">
-
-        <div className="footer-desktop-logo-acces">
-          
-          <a href="/"><img className="logo_footer_desktop" src="/logos/logo-principal-bleu.png"/></a>
-
-          <div className="footer-desktop-contact">
-            <h3>CONTACT</h3>
-            <p>Mail : contact@optimom-conseil.com</p>
-            <p><a href="tel:0146228185">Tel : +.33.(0)1.46.22.81.85</a></p>
-            <br></br>
-          </div>
-
-          <div className="footer-desktop-access">
-            <h3>ADRESSE</h3>
-            <p>SAS OPTIMOM CONSEIL</p>
-            <p>272 rue du Faubourg Saint Honoré, </p>
-            <p>75008 Paris</p><br></br>   
-          </div>
-              
-          
-                   
-        </div>
-
-        <hr width="80%"></hr>
-
-        <div className="footer-lien">
-            <div>
-              <h3>DOCUMENTATION LÉGALE</h3>
-                <p><a href="">Attestation d'adhésion CNCEF Patrimoine</a></p>
-                <p><a href="">Attestation d'assurance RCP</a></p>
-                <p><a href="">Attestation d'inscription ORIAS</a></p>
+          {/* Colonne 1 : Logo + Docs */}
+          <div className="footer-block-top">
+            <div className="footer-top">
+              <img href="/" className="logo_footer_desktop" src="/logos/logo-principal-bleu.png" alt="Logo" />
             </div>
-          <div className="footer-desktop-infos">
-             <h3>INFORMATIONS LÉGALES</h3>
+            <div className="footer-top">
+              <h3>CONTACT</h3>
+              <ul>
+                <li>contact@optimom-conseil.com</li>
+                <li><a href="tel:0146228185">+33 (0)1 46 22 81 85</a></li>
+                <li></li>
+              </ul>
+            </div>
+            <div className="footer-top">
+              <h3>ADRESSE</h3>
+              <ul>
+                <li>SAS OPTIMOM CONSEIL</li>
+                <li>272 rue du Faubourg Saint Honoré</li>
+                <li>75008 Paris</li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Colonne 2 : Contact + Mentions */}
+          <div className="footer-block-bottom">
+            
+            <div className="footer-bottom">
+              <h3>INFORMATIONS LÉGALES</h3>
               <ul>
                 <li><a href="/mentions-legales">Mentions légales</a></li>
               </ul>
             </div>
-            <div className="footer-desktop-liens-utiles">
+            <div className="footer-bottom">
+              <h3>DOCUMENTATION LÉGALE</h3>
+              <ul>
+                <li><a href="#">Attestation d'adhésion CNCEF Patrimoine</a></li>
+                <li><a href="#">Attestation d'adhésion CNCEF Patrimoine</a></li>
+                <li><a href="#">Attestation d'inscription ORIAS</a></li>
+              </ul>
+            </div>
+            <div className="footer-bottom">
               <h3>LIENS UTILES</h3>
               <ul>
-                <li>            
-                <NavLink to="/">Accueil</NavLink>
-                </li>
-                <li>
-                <NavLink to="/expertises">Nos expertises</NavLink>
-                </li>
-                <li>
-                <NavLink to="/contact">Contact</NavLink>
-                </li>      
-            </ul>
-            </div>     
-
-        </div>
-        
+                <li><NavLink to="/">Accueil</NavLink></li>
+                <li><NavLink to="/expertises">Nos expertises</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+              </ul>
+            </div>
+          </div>
+          
       </div>
+
+
 
       {/* Version Mobile avec dropdown */}
       <div className="footer-mobile">
--- frontend/optimom-conseil/src/views/Contact.jsx
+++ frontend/optimom-conseil/src/views/Contact.jsx
@@ -12,7 +12,7 @@ function Contact () {
             children="Contactez-nous"
             />
             <ContactForm/>
-            <Footer/>
+            <Footer />
         </>
     )
 }
